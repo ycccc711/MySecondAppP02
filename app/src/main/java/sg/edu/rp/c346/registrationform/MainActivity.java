bec24b4f0aaadf8116a748f4cf53c1814b4d41e0
ER YI CHEN <17038568@rp.edu.sg>
2019-04-30 15:06:54 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/registrationform/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.registrationform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enhancement);
+    }
+}
